893f68d1bbe1b118a78918eb03c5b40c5efbac1a
anita paudel <anitapau@uw.edu>
2017-02-12 21:03:34 -0800

    person added

--- Person.java
+++ Person.java
@@ -1,1 +1,33 @@
-public 
+
+public class Person {
+	String userName;	
+	String password;
+	String authentication;
+	public Person(){
+		
+	}
+	public Person(String theUserName, String thePassword, String theAuthentication) {
+		this.userName = theUserName;
+		this.password = thePassword;
+		this.authentication = theAuthentication;
+	}
+	
+	public String getUserName() {
+		return userName;
+	}
+	public void setAuthentication(String theAuthentication) {
+		this.authentication = theAuthentication;
+	}
+	public String getAuthentication() {
+		return authentication;
+	}
+	public void setUserName(String theUserName) {
+		this.userName = theUserName;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String thePassword) {
+		this.password = thePassword;
+	}
+}
